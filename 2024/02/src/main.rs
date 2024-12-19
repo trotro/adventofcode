6ef6f506118419f5ec5a91363d76b260eaf4bba7
Nicolas TRAUWAEN <trotro@users.noreply.github.com>
2024-12-19 15:22:01 +0100

    Day 2 part 2

--- 2024/02/src/main.rs
+++ 2024/02/src/main.rs
@@ -12,55 +12,44 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn is_safe(levels: Vec<i32>) -> bool {
-    let mut safe: bool = false;
-    let mut increase: bool = false;
-    let mut decrease: bool = false;
-    for i in 1..levels.len() {
-        if levels[i] != levels[i - 1] {
-            for i in 1..levels.len() {
-                match levels[i] {
-                    val if val == levels[i - 1] + 1 => {
-                        increase = true;
-                        safe = true;
-                    }
-                    val if val == levels[i - 1] + 2 => {
-                        increase = true;
-                        safe = true;
-                    }
-                    val if val == levels[i - 1] + 3 => {
-                        increase = true;
-                        safe = true;
-                    }
-                    val if val == levels[i - 1] - 1 => {
-                        decrease = true;
-                        safe = true;
-                    }
-                    val if val == levels[i - 1] - 2 => {
-                        decrease = true;
-                        safe = true;
-                    }
-                    val if val == levels[i - 1] - 3 => {
-                        decrease = true;
-                        safe = true;
-                    }
-                    _ => {
-                        return false;
-                    }
-                }
-                if increase && decrease {
-                    return false;
-                }
-            }
-        } else {
-            return false;
-        }
+fn is_safe(report: &Vec<i32>) -> bool {
+    if report.is_sorted_by(|a, b| a < b && b - a < 4) {
+        return true;
     }
-    return safe;
+    if report.is_sorted_by(|a, b| a > b && a - b < 4) {
+        return true;
+    }
+    return false;
+}
+
+fn problem_dampener(mut report: Vec<i32>) -> i32 {
+    if is_safe(&report) {
+        return 0;
+    }
+    let (_, mut levels) = report.split_first_mut().unwrap();
+    if is_safe(&levels.to_vec()) {
+        return 1;
+    }
+    (_, levels) = report.split_last_mut().unwrap();
+    if is_safe(&levels.to_vec()) {
+        return 1;
+    }
+    for i in 0..report.len() {
+        let mut report_without_wrong_level = report.clone();
+        report_without_wrong_level.remove(i);
+        if is_safe(&report_without_wrong_level) {
+            return 1;
+        }
+        println!("Report is still wrong: {:?}", report_without_wrong_level)
+    }
+    return 2;
 }
 
 fn main() {
-    let mut safe: i32 = 0;
+    let mut count_safe: i32 = 0;
+    let mut count_dampener_safe: i32 = 0;
+    let mut unsafe_reports: i32 = 0;
+    let mut nb_reports: i32 = 0;
     if let Ok(reports) = read_lines("./input") {
         // Consumes the iterator, returns an (Optional) String
         for report in reports.flatten() {
@@ -68,10 +57,22 @@ fn main() {
                 .split_whitespace()
                 .map(|l| l.parse::<i32>().unwrap())
                 .collect();
-            if is_safe(levels) {
-                safe += 1;
+            match problem_dampener(levels) {
+                0 => {
+                    count_safe += 1;
+                    count_dampener_safe += 1
+                }
+                1 => count_dampener_safe += 1,
+                _ => unsafe_reports += 1,
             }
+            nb_reports += 1;
         }
+        println!("Total reports: {}", nb_reports);
+        println!("Safe reports: {}", count_safe);
+        println!(
+            "Safe reports after Problem Dampener: {}",
+            count_dampener_safe
+        );
+        println!("Unsafe reports: {}", unsafe_reports);
     }
-    println!("Safe reports: {}", safe);
 }
