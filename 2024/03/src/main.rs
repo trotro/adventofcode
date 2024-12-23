cfeed8bc2d1f14744a89988eaa4ab65bd72c2a74
Nicolas TRAUWAEN <trotro@users.noreply.github.com>
2024-12-23 18:26:19 +0100

    Day 3 part 2

--- 2024/03/src/main.rs
+++ 2024/03/src/main.rs
@@ -1,34 +1,35 @@
 use regex::Regex;
 use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io::{self, Read};
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+fn read_lines(filename: &str) -> io::Result<String> {
+    let mut file = File::open(filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
 }
 
-fn extract_numbers(input: &String) -> Vec<i32> {
+fn part1(memory: &str) -> usize {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let caps = re.captures_iter(input).map(|n| {
-        let num1 = n.get(1).unwrap().as_str().parse::<i32>().unwrap();
-        let num2 = n.get(2).unwrap().as_str().parse::<i32>().unwrap();
-        num1 * num2
-    });
-    return caps.collect();
+    return re
+        .captures_iter(memory)
+        .filter(|n| !n.get(1).is_none())
+        .map(|n| n[1].parse::<usize>().unwrap() * n[2].parse::<usize>().unwrap())
+        .sum();
+}
+
+fn part2(memory: &str) -> usize {
+    let re: Regex = Regex::new(r"(?s)don't\(\).*?(?:do\(\)|$)|mul\((\d+),(\d+)\)").unwrap();
+    return re
+        .captures_iter(memory)
+        .filter(|n| !n.get(1).is_none())
+        .map(|n| n[1].parse::<usize>().unwrap() * n[2].parse::<usize>().unwrap())
+        .sum();
 }
 
 fn main() {
-    let mut sum: i32 = 0;
     if let Ok(instructions) = read_lines("./input") {
-        // Consumes the iterator, returns an (Optional) String
-        for line in instructions.flatten() {
-            let extracted_numbers = extract_numbers(&line);
-            sum += extracted_numbers.iter().sum::<i32>();
-        }
+        println!("Part 1: {}", part1(&instructions));
+        println!("Part 2: {}", part2(&instructions));
     }
-    println!("Result is {}", sum)
 }
